2ef5edc56cdb2c781e3f4a17450374098975f81c
CORENTIN ROSSETTO <114078357+Rococo77@users.noreply.github.com>
2024-04-16 15:00:30 +0200

    first commit

---
+++ Mediatheque.Core/Service/MediathequeService.cs
@@ -0,0 +1,48 @@
+﻿using Mediatheque.Core.Model;
+
+namespace Mediatheque.Core.Service
+{
+    public class MediathequeService
+    {
+        private List<ObjetDePret> _fondDeCommerce = new List<ObjetDePret>();
+        private INotationService _notationService;
+
+        public MediathequeService(INotationService notationService)
+        {
+            _notationService = notationService;
+        }
+
+        public void AddObjet(ObjetDePret objet)
+        {
+            _fondDeCommerce.Add(objet);
+        }
+
+        public int GetNombreObjet()
+        {
+            return _fondDeCommerce.Count;
+        }
+
+        public List<string> PresentationCD()
+        {
+            List<string> descriptionsCDs = new List<string>();
+
+            if (this.GetNombreObjet() == 0)
+            {
+                descriptionsCDs.Add("Pas de CD à afficher");
+            } else
+            {
+                descriptionsCDs.Add("Presentation des CDs:");
+            }
+
+            for (int i = 0; i < _fondDeCommerce.Count; i++)
+            {
+                if (_fondDeCommerce[i] is CD)
+                {
+                    descriptionsCDs.Add($"Album: {_fondDeCommerce[i].TitreDeLObjet} Note: {_notationService.GetNoteAlbum(_fondDeCommerce[i].TitreDeLObjet)}");
+                };
+            }
+
+            return descriptionsCDs;
+        }
+    }
+}
---
+++ Mediatheque.CoreTests/Service/MediathequeServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mediatheque.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediatheque.Core.Model;
+
+namespace Mediatheque.Core.Service.Tests
+{
+    [TestClass()]
+    public class MediathequeServiceTests
+    {
+        [TestMethod()]
+        public void PresentationCDTest(INotationService notationService)
+        {
+            // Arrange
+            var cd = new CD("Yuusha", "Yoasobi");
+
+            // Act
+            MediathequeService mediatheque = new MediathequeService(notationService);
+            mediatheque.AddObjet(cd);
+
+            // Assert
+            List<string> expected = new List<string>();
+            expected.Add("Présentation des CDs :");
+            expected.Add("Album : Yuusha Notation : ");
+
+            CollectionAssert.AreEqual(expected, mediatheque.PresentationCD());
+        }
+    }
+}
